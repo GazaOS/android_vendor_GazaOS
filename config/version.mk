7ab2d2286f4d2f235d4e857ab3f2da85ffe48908
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-06-09 07:16:41 +0600

    GazaOS: added some more

--- config/version.mk
+++ config/version.mk
@@ -1,3 +1,6 @@
+# Include cr.mk to Set BUILD_TYPEAdd commentMore actions
+include vendor/lineage/config/crdroid.mk
+
 PRODUCT_VERSION_MAJOR = 15
 PRODUCT_VERSION_MINOR = 0
 
@@ -5,8 +8,7 @@ PRODUCT_VERSION_MINOR = 0
 CR_VERSION := 11.6
 
 # Internal version
-LINEAGE_VERSION := GazaOS-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(CR_VERSION)
-
+LINEAGE_VERSION := GazaOS-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date +%Y%m%d)-$(LINEAGE_BUILD)-v$(CR_VERSION)-$(BUILD_TYPE)
 # Display version
 LINEAGE_DISPLAY_VERSION := v$(CR_VERSION)-$(shell date +%Y%m%d)
 
